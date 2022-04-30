8b7c8be67f397c65369e956fce96a0ddfb38bb1c
dipesh <dipeshbhowar001@gmail.com>
2022-04-30 10:14:24 +0530

    updated servies.php

--- config/services.php
+++ config/services.php
@@ -31,8 +31,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'facebook' => [
-        'client_id' => '',
-        'client_secret' => '',
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
         'redirect' => 'http://localhost:8000/auth/facebook/callback',
     ],
 
